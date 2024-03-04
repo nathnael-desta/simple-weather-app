b90d5f2a49a61b7cbed278d37ba0747d2f0a038c
nathnael-desta <120774109+nathnael-desta@users.noreply.github.com>
2024-03-04 03:27:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# basic-template
+webpack, linitng and prettier
+
+Based on content taken from the odin project
+
+Webpack
+https://webpack.js.org/guides/getting-started/
+
+Eslint
+https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+https://www.digitalocean.com/community/tutorials/linting-and-formatting-with-eslint-in-vs-code
+
+Prettier
+https://prettier.io/
+https://github.com/prettier/eslint-config-prettier#installation
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.ejs'
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
